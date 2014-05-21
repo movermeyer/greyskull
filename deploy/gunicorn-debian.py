1f40ad05509a06efa17364693c5fba2de3516f49
Carlos Killpack <carlos@zulumarketing.com>
2014-05-21 09:36:17 -0600

    Tornado scaffolding

--- deploy/gunicorn-debian.py
+++ deploy/gunicorn-debian.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
 CONFIG = {
-    'mode': 'wsgi',
+    'mode': 'wsgi',  # FIXME is this right?
     'environment': {
         #'PYTHONPATH': '/path/to/virtualenv
     },
     'args': (
         '--bind=unix:/var/run/gunicorn/greyskull.sock',
         '--workers=4',
-        '--worker-class=gevent',
-        '--worker-connections=30',
+        '--worker-class=tornado',
         '--timeout=30',
-        'greyskull.wsgi:application'
+        'greyskull:application'
     )
 }
--- setup.py
+++ setup.py
@@ -3,6 +3,24 @@
 
 from setuptools import setup
 
+from greyskull import (__version__ as version,
+                       __license__ as license,
+                       __author__ as author,
+                       __email__ as author_email)
+
 setup(
-    
+    name="Greyskull",
+    version=version,
+    description="A lightweight bittorrent tracker",
+    long_description=open("README.txt", 'rb').read().decode('utf-8'),
+    license=license,
+    author=author,
+    author_email=author_email,
+    packages=[
+        'greyskull',
+    ],
+    install_requires=[
+        'tornado',
+    ],
+    classifiers=[]
 )
